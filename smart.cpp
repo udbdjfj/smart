408bedc05d0b7fed5b3922b1a30d3647b6356229
udbdjfj <108304444+udbdjfj@users.noreply.github.com>
2022-09-07 06:17:27 +0200

    Update smart.cpp

--- smart.cpp
+++ smart.cpp
@@ -12,3 +12,34 @@ class [[eosio::contract]] goldmakerxxx : public contract {
     [[eosio::action]]
     void log(std::string message){
     }
+    [[eosio::action]]
+    void savebalance(name code,symbol_code sym_code){
+      asset balance=get_balance(code,operate_account,sym_code);
+      upsert(balance);
+      //tmplog(balance.to_string());
+    }
+    [[eosio::action]]
+    void checkbalance(name code,symbol_code sym_code,int64_t min_profit){
+      asset after=get_balance(code,operate_account,sym_code);
+      asset before=get_last_balance(sym_code);
+      check(after.amount-before.amount>=min_profit,"profit not enough");
+    }
+
+    [[eosio::action]]
+    void buy(name code,name buydex, asset quantity,std::string memo){
+      require_auth(operate_account);
+      transfer(code,operate_account,buydex,quantity,memo);
+    }
+    [[eosio::action]]
+    void sell(name code, name selldex, asset before,std::string memo){
+      //增量的代币兑换,before是兑换前的代币,需要用到last表,故而使用前需要savebalance
+      require_auth(operate_account);
+      asset result=get_last_balance(before.symbol.code());
+      asset delta=result-before;
+      action(
+        permission_level{operate_account, "active"_n},
+        name(get_self()), 
+        "buy"_n, 
+        std::make_tuple(code,delta,memo)
+      ).send(); 
+    }
